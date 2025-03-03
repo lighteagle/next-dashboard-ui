550b15d879073efc9eb01123255a3e05b663a6af
lighteagle <datagisss18@gmail.com>
2025-03-03 19:43:54 +0700

    form modal

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,37 +1,43 @@
-import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { assignmentsData, examsData, role } from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
 import { access } from "fs";
 import Image from "next/image";
 import Link from "next/link";
 
-type Assignment = {
+type Event = {
   id: number;
-  subject: string;
+  title: string;
   class: string;
-  teacher: string;
-  dueDate: string;
+  date: string;
+  startTime: string;
+  endTime: string;
 };
 
 const columns = [
   {
-    header: "Subject Name",
+    header: "Title",
     accessor: "name",
   },
   {
     header: "Class",
     accessor: "class",
-  },
-  {
-    header: "Teacher",
-    accessor: "teacher",
     className: "hidden md:table-cell",
   },
   {
-    header: "Due Date",
-    accessor: "dueDate",
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
     className: "hidden md:table-cell",
   },
   {
@@ -40,23 +46,28 @@ const columns = [
   },
 ];
 
-const AsignmentListPage = () => {
-  const renderRow = (item: Assignment) => (
+const EventListPage = () => {
+  const renderRow = (item: Event) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.dueDate}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
       <td>
         <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/edit.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
           {role === "admin" && (
-            <>
-              <FormModal table="assignment" type="update" data={item} />
-              <FormModal table="assignment" type="delete" id={item.id} />
-            </>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
           )}
         </div>
       </td>
@@ -67,9 +78,7 @@ const AsignmentListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">
-          All Assignments
-        </h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -79,16 +88,20 @@ const AsignmentListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center bg-lamaYellow rounded-full">
               <Image src="/sort.png" alt="sort" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="assignment" type="create" />}
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center bg-lamaYellow rounded-full">
+                <Image src="/plus.png" alt="add" width={14} height={14} />
+              </button>
+            )}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
+      <Table columns={columns} renderRow={renderRow} data={eventsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default AsignmentListPage;
+export default EventListPage;
